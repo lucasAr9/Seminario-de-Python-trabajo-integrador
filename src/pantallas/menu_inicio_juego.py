95df25720380654abc4517779200ee5ccca56ac9
Antonella Denisse Cuenca <denisseanto11@hotmail.com>
2022-05-17 15:29:18 -0300

    Cambio de imagenes y modificaciones en la clase del menu principal.

--- src/pantallas/menu_inicio_juego.py
+++ src/pantallas/menu_inicio_juego.py
@@ -12,13 +12,17 @@ FONT_COMBO = 'Verdana 18'
 
 
 class MenuInicio:
-    def __init__(self, usuarios=['user1', 'user2']):
+    def __init__(self, usuarios=None):
         """
         :param usuarios: es la lista que genera la ventana de 'Pantalla de creación/edición de perfil'.
         """
         self._lista_usuarios = usuarios
 
     @property
+    def lista_usuarios(self):
+        return self._lista_usuarios
+
+    @lista_usuarios.setter
     def lista_usuarios(self, usuarios):
         self._lista_usuarios = usuarios
 
@@ -42,7 +46,7 @@ class MenuInicio:
         :return: el layout que contendra la selccion de usuario y dificultad
         """
         # ruta para la imagen
-        ruta_imagen = os.path.join(os.path.realpath('..'), "recursos", "imagenes", "juego-de-cartas.png")
+        ruta_imagen = os.path.join(os.path.realpath('..'), "recursos", "imagenes", "imagen_menu_principal.png")
         # layout
         layout = [
             [sg.Combo(self._lista_usuarios, key='-USUARIOS-', default_value="Seleccione su usuario", enable_events=True,
@@ -61,16 +65,18 @@ class MenuInicio:
         dos columnas con (_layout_op_principales(self) y
         _layout_usuario_dificultad(self)) y
         un Boton con el cual se cierra la ventana.
+        :return: la ventana del menu principal
         """
         sg.theme('DarkAmber')
         # rutas para las imagenes
         ruta_titlebar_icon = os.path.join(os.path.realpath('..'), "recursos", "imagenes", "cartas_icon.png")
         ruta_icon = os.path.join(os.path.realpath('..'), "recursos", "imagenes", "cartas_icon.ico")
+        ruta_image_titulo = os.path.join(os.path.realpath('..'), "recursos", "imagenes", "titulo_figurace.png")
         # layouts 
         l_principal = self._layout_op_principales()
         l_user_dif = self._layout_usuario_dificultad()
         layout = [
-            [sg.Push(), sg.Text("FIGURACE", justification='center', expand_x=True, font=FONT_TITULO), sg.Push()],
+            [sg.Push(), sg.Image(ruta_image_titulo), sg.Push()],
             [sg.Push(), sg.Col(l_principal, size=TAM_COLUMNAS, expand_y=True), sg.Col(l_user_dif,  size=TAM_COLUMNAS, 
             expand_y=True, element_justification='center'), sg.Push()],
             [sg.Push(), sg.Button("Salir", key='-SALIR-', font=FONT_BOTONES), sg.Push()]
